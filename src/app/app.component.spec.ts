219592e123e0b8d9e89a79da3472f44e08edc197
simranjeet <singh2018developer@gmail.com>
2018-04-21 15:40:36 +0530

    first commit
    todo list app start to finish

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,54 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+      providers: [TodoService]
+    });
   }));
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    todoService = fixture.debugElement.injector.get(TodoService);
+  }));
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+
+  it(`should have a newTodo todo`, async(() => {
+    expect(app.newTodo instanceof Todo).toBeTruthy()
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+
+  it('should display "Todos" in h1 tag', async(() => {
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Todos');
+  }));
+
+  it('should add a todo', async(() => {
+    spyOn(todoService, 'addTodo');
+    app.addTodo();
+    expect(todoService.addTodo).toHaveBeenCalled();
+  }));
+
+  it('should complete a todo', async(() => {
+    spyOn(todoService, 'toggleTodoComplete');
+    app.toggleTodoComplete();
+    expect(todoService.toggleTodoComplete).toHaveBeenCalled();
+  }));
+
+  it('should remove a todo', async(() => {
+    spyOn(todoService, 'deleteTodoById');
+    app.removeTodo(1);
+    expect(todoService.deleteTodoById).toHaveBeenCalled();
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
+import { TodoService } from './todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoService]
 })
 export class AppComponent {
-  title = 'app';
+  
+  newTodo: Todo = new Todo();
+
+  constructor(private todoService: TodoService) {
+  }
+
+  addTodo() {
+    this.todoService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoService.getAllTodos();
+  }
+
 }
